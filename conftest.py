40f279fad55828413e3eb571ac432cf7a8f02e01
KKaries <hrntsvkirill@gmail.com>
2023-03-20 11:58:07 +0200

    Feedback form 3 x TCs were added

--- conftest.py
+++ conftest.py
@@ -1,9 +1,27 @@
 import pytest
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser',
+                    action='store',
+                    default='chrome',
+                    help='Choose browser: chrome, firefox or edge')
 
 
 @pytest.fixture(scope="function")
-def browser():
-    browser = webdriver.Chrome()
+def browser(request):
+    browser_name = request.config.getoption('--browser')
+    if browser_name == 'chrome':
+        browser = webdriver.Chrome(ChromeDriverManager().install())
+    elif browser_name == 'firefox':
+        browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    elif browser_name == 'edge':
+        browser = webdriver.Edge(EdgeChromiumDriverManager().install())
+    else:
+        raise pytest.UsageError('==browser_name should be chrome, firefox or edge')
     yield browser
     browser.quit()
--- test_create_users.py
+++ test_create_users.py
@@ -153,3 +153,320 @@ class TestsCreateUsers:
         browser.switch_to.window(w_agiles)
         browser.get(access_link)
         wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.WhatsNewModal__modalTitle')))
+
+
+class TestFeedbackForm:
+    @pytest.mark.smoke
+    def test_smoke_form_opens(self, browser):
+        wait = WebDriverWait(browser, 20)
+
+        # Open http://localhost:3000/:
+        browser.get(link_home)
+
+        # Wait until "Email" input field is loaded on the page:
+        wait.until(EC.presence_of_element_located(
+            (By.XPATH, '//input[@placeholder="Enter Your Email"]'))
+        )
+
+        # Fill in "Email" field:
+        browser.find_element(By.XPATH, '//input[@placeholder="Enter Your Email"]').send_keys('board_owner@mail.com')
+
+        # Fill in "Password" field:
+        browser.find_element(By.XPATH, '//input[@placeholder="Enter Your Password"]').send_keys('Godfather1989!')
+
+        # Click "Sign in" button:
+        browser.find_element(By.CSS_SELECTOR, '.Button__button.Button__blue.styles__actionButton').click()
+
+        # Wait for "Not Found" alert to be displayed:
+        wait.until(EC.presence_of_element_located(
+            (By.XPATH, '//div[@role="alert"]')
+        ))
+
+        # Close "Not Found" alert:
+        if len(browser.find_elements(By.XPATH, '//div[@role="alert"]')) != 0:
+            browser.find_element(By.XPATH,
+                                 '//*[@id="root"]/div[2]/div/div/div/div/div/button').click()
+
+        # Click "Got it" if modal wdw "What's New" is opened(button appears):
+        if len(browser.find_elements(By.XPATH, '/html/body/div[2]/div/div[2]/div/div[2]/div[3]/button')) != 0:
+            browser.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/div/div[2]/div[3]/button').click()
+
+        # Click "Retrospective" tab:
+        browser.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[1]/nav/a[3]/div').click()
+
+        # Click "Feedback" button:
+        browser.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[1]/nav/div[3]/button[1]/div').click()
+
+        # Check if opened dialog title is displayed:
+        wait.until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, '.ant-modal-header')
+        ))
+        assert browser.find_element(By.CSS_SELECTOR, '.ant-modal-header').text in 'AGILES APP FEEDBACK SURVEY',\
+            'Feedback Form was not opened'
+
+    @pytest.mark.smoke
+    def test_smoke_form_submits(self, browser):
+        wait = WebDriverWait(browser, 20)
+
+        # Open http://localhost:3000/:
+        browser.get(link_home)
+
+        # Wait until "Email" input field is loaded on the page:
+        wait.until(EC.presence_of_element_located(
+            (By.XPATH, '//input[@placeholder="Enter Your Email"]'))
+        )
+
+        # Fill in "Email" field:
+        browser.find_element(By.XPATH, '//input[@placeholder="Enter Your Email"]').send_keys('board_owner@mail.com')
+
+        # Fill in "Password" field:
+        browser.find_element(By.XPATH, '//input[@placeholder="Enter Your Password"]').send_keys('Godfather1989!')
+
+        # Click "Sign in" button:
+        browser.find_element(By.CSS_SELECTOR, '.Button__button.Button__blue.styles__actionButton').click()
+
+        # Wait for "Not Found" alert to be displayed:
+        wait.until(EC.presence_of_element_located(
+            (By.XPATH, '//div[@role="alert"]')
+        ))
+
+        # Close "Not Found" alert:
+        if len(browser.find_elements(By.XPATH, '//div[@role="alert"]')) != 0:
+            browser.find_element(By.XPATH,
+                                 '//*[@id="root"]/div[2]/div/div/div/div/div/button').click()
+
+        # Click "Got it" if modal wdw "What's New" is opened(button appears):
+        if len(browser.find_elements(By.XPATH, '/html/body/div[2]/div/div[2]/div/div[2]/div[3]/button')) != 0:
+            browser.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/div/div[2]/div[3]/button').click()
+
+        # Click "Retrospective" tab:
+        browser.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[1]/nav/a[3]/div').click()
+
+        # Click "Feedback" button:
+        browser.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[1]/nav/div[3]/button[1]/div').click()
+
+        # Check if opened dialog title is displayed:
+        wait.until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, '.ant-modal-header')
+        ))
+
+        # Click on 1 scale button:
+        browser.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/div/div[2]/div[2]/ol/li[1]/label/div').click()
+
+        # Click on "Send" button:
+        browser.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/div/div[2]/div[3]/button[2]').click()
+
+        # Check if Succsessfull message appeared:
+        wait.until(EC.presence_of_element_located(
+            (By.XPATH, '//*[@id="root"]/div[2]/div/div/div/div')
+        ))
+        assert browser.find_element(By.XPATH, '//*[@id="root"]/div[2]/div/div/div/div/div/div/div').text in\
+               'Your feedback has been submitted. Thank you!', 'Incorrect Successful message'
+
+    @pytest.mark.feedbacksymbolslimit
+    def test_5246_allowed_limit_254(self, browser):
+        wait = WebDriverWait(browser, 20)
+
+        # Open http://localhost:3000/:
+        browser.get(link_home)
+
+        # Wait until "Email" input field is loaded on the page:
+        wait.until(EC.presence_of_element_located(
+            (By.XPATH, '//input[@placeholder="Enter Your Email"]'))
+        )
+
+        # Fill in "Email" field:
+        browser.find_element(By.XPATH, '//input[@placeholder="Enter Your Email"]').send_keys('board_owner@mail.com')
+
+        # Fill in "Password" field:
+        browser.find_element(By.XPATH, '//input[@placeholder="Enter Your Password"]').send_keys('Godfather1989!')
+
+        # Click "Sign in" button:
+        browser.find_element(By.CSS_SELECTOR, '.Button__button.Button__blue.styles__actionButton').click()
+
+        # Wait for "Not Found" alert to be displayed:
+        wait.until(EC.presence_of_element_located(
+            (By.XPATH, '//div[@role="alert"]')
+        ))
+
+        # Close "Not Found" alert:
+        if len(browser.find_elements(By.XPATH, '//div[@role="alert"]')) != 0:
+            browser.find_element(By.XPATH,
+                                 '//*[@id="root"]/div[2]/div/div/div/div/div/button').click()
+
+        # Click "Got it" if modal wdw "What's New" is opened(button appears):
+        if len(browser.find_elements(By.XPATH, '/html/body/div[2]/div/div[2]/div/div[2]/div[3]/button')) != 0:
+            browser.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/div/div[2]/div[3]/button').click()
+
+        # Click "Retrospective" tab:
+        browser.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[1]/nav/a[3]/div').click()
+
+        # Click "Feedback" button:
+        browser.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[1]/nav/div[3]/button[1]/div').click()
+
+        # Check if opened dialog title is displayed:
+        wait.until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, '.ant-modal-header')
+        ))
+
+        # Click on 1 scale button:
+        browser.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/div/div[2]/div[2]/ol/li[1]/label/div').click()
+
+        # Check If "Send" button becomes available:
+        assert browser.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/div/div[2]/div[3]/button[2]').is_enabled()
+
+        # Put 254 symbols into input field:
+        input_f = browser.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/div/div[2]/div[2]/div[3]/div/textarea')
+        input_f\
+            .send_keys('Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.'
+                       ' Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus'
+                       ' mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis')
+
+        # Check if input field received 254 symbols:
+        assert len(input_f.text) == 254, f'Error - length is {len(input_f.text)} instead of 254 symbols'
+
+        # Click on "Send" button:
+        browser.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/div/div[2]/div[3]/button[2]').click()
+
+        # Check if Succsessfull message appeared:
+        wait.until(EC.presence_of_element_located(
+            (By.XPATH, '//*[@id="root"]/div[2]/div/div/div/div')
+        ))
+        assert browser.find_element(By.XPATH, '//*[@id="root"]/div[2]/div/div/div/div/div/div/div').text in\
+               'Your feedback has been submitted. Thank you!', 'Incorrect Successful message'
+
+    @pytest.mark.feedbacksymbolslimit
+    def test_5246_allowed_limit_255(self, browser):
+        wait = WebDriverWait(browser, 20)
+
+        # Open http://localhost:3000/:
+        browser.get(link_home)
+
+        # Wait until "Email" input field is loaded on the page:
+        wait.until(EC.presence_of_element_located(
+            (By.XPATH, '//input[@placeholder="Enter Your Email"]'))
+        )
+
+        # Fill in "Email" field:
+        browser.find_element(By.XPATH, '//input[@placeholder="Enter Your Email"]').send_keys('board_owner@mail.com')
+
+        # Fill in "Password" field:
+        browser.find_element(By.XPATH, '//input[@placeholder="Enter Your Password"]').send_keys('Godfather1989!')
+
+        # Click "Sign in" button:
+        browser.find_element(By.CSS_SELECTOR, '.Button__button.Button__blue.styles__actionButton').click()
+
+        # Wait for "Not Found" alert to be displayed:
+        wait.until(EC.presence_of_element_located(
+            (By.XPATH, '//div[@role="alert"]')
+        ))
+
+        # Close "Not Found" alert:
+        if len(browser.find_elements(By.XPATH, '//div[@role="alert"]')) != 0:
+            browser.find_element(By.XPATH,
+                                 '//*[@id="root"]/div[2]/div/div/div/div/div/button').click()
+
+        # Click "Got it" if modal wdw "What's New" is opened(button appears):
+        if len(browser.find_elements(By.XPATH, '/html/body/div[2]/div/div[2]/div/div[2]/div[3]/button')) != 0:
+            browser.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/div/div[2]/div[3]/button').click()
+
+        # Click "Retrospective" tab:
+        browser.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[1]/nav/a[3]/div').click()
+
+        # Click "Feedback" button:
+        browser.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[1]/nav/div[3]/button[1]/div').click()
+
+        # Check if opened dialog title is displayed:
+        wait.until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, '.ant-modal-header')
+        ))
+
+        # Click on 1 scale button:
+        browser.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/div/div[2]/div[2]/ol/li[1]/label/div').click()
+
+        # Check If "Send" button becomes available:
+        assert browser.find_element(By.XPATH,
+                                    '/html/body/div[2]/div/div[2]/div/div[2]/div[3]/button[2]').is_enabled()
+
+        # Put 255 symbols into input field:
+        input_field = browser.find_element(By.XPATH,
+                                           '/html/body/div[2]/div/div[2]/div/div[2]/div[2]/div[3]/div/textarea')
+        input_field \
+            .send_keys('Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.'
+                       ' Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus'
+                       ' mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis1')
+
+        # Check if input field received 255 symbols:
+        assert len(input_field.text) == 255, f'Error - length is {len(input_field.text)} instead of 255 symbols'
+
+        # Click on "Send" button:
+        browser.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/div/div[2]/div[3]/button[2]').click()
+
+        # Check if Succsessfull message appeared:
+        wait.until(EC.presence_of_element_located(
+            (By.XPATH, '//*[@id="root"]/div[2]/div/div/div/div')
+        ))
+        assert browser.find_element(By.XPATH, '//*[@id="root"]/div[2]/div/div/div/div/div/div/div').text in \
+               'Your feedback has been submitted. Thank you!', 'Incorrect Successful message'
+
+    @pytest.mark.feedbacksymbolslimit
+    def test_5247_not_allowed_limit(self, browser):
+        wait = WebDriverWait(browser, 20)
+
+        # Open http://localhost:3000/:
+        browser.get(link_home)
+
+        # Wait until "Email" input field is loaded on the page:
+        wait.until(EC.presence_of_element_located(
+            (By.XPATH, '//input[@placeholder="Enter Your Email"]'))
+        )
+
+        # Fill in "Email" field:
+        browser.find_element(By.XPATH, '//input[@placeholder="Enter Your Email"]').send_keys('board_owner@mail.com')
+
+        # Fill in "Password" field:
+        browser.find_element(By.XPATH, '//input[@placeholder="Enter Your Password"]').send_keys('Godfather1989!')
+
+        # Click "Sign in" button:
+        browser.find_element(By.CSS_SELECTOR, '.Button__button.Button__blue.styles__actionButton').click()
+
+        # Wait for "Not Found" alert to be displayed:
+        wait.until(EC.presence_of_element_located(
+            (By.XPATH, '//div[@role="alert"]')
+        ))
+
+        # Close "Not Found" alert:
+        if len(browser.find_elements(By.XPATH, '//div[@role="alert"]')) != 0:
+            browser.find_element(By.XPATH,
+                                 '//*[@id="root"]/div[2]/div/div/div/div/div/button').click()
+
+        # Click "Got it" if modal wdw "What's New" is opened(button appears):
+        if len(browser.find_elements(By.XPATH, '/html/body/div[2]/div/div[2]/div/div[2]/div[3]/button')) != 0:
+            browser.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/div/div[2]/div[3]/button').click()
+
+        # Click "Retrospective" tab:
+        browser.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[1]/nav/a[3]/div').click()
+
+        # Click "Feedback" button:
+        browser.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[1]/nav/div[3]/button[1]/div').click()
+
+        # Check if opened dialog title is displayed:
+        wait.until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, '.ant-modal-header')
+        ))
+
+        # Put 256 symbols into input field:
+        input_field = browser.find_element(By.XPATH,
+                                           '/html/body/div[2]/div/div[2]/div/div[2]/div[2]/div[3]/div/textarea')
+        input_field \
+            .send_keys('Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.'
+                       ' Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus'
+                       ' mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis12')
+
+        # Check if input field have not received 256 symbols:
+        assert len(input_field.text) == 255, f'Error - length is {len(input_field.text)} instead of 255 max allowed'
+
+        # Check If "Send" button does not become available:
+        assert not browser.find_element(By.XPATH,
+                                    '/html/body/div[2]/div/div[2]/div/div[2]/div[3]/button[2]').is_enabled(), \
+            '"Send" button became available without scaling'
