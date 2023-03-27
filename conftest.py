8ae453b2fbad02437a2c176daca3a1fa5966f598
KKaries <hrntsvkirill@gmail.com>
2023-03-27 14:16:14 +0300

    Tests update (parametrization added), Database check implemented

--- conftest.py
+++ conftest.py
@@ -1,12 +1,32 @@
 import pytest
+import openpyxl
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from selenium.webdriver.edge.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from testdata import *
+from sqlalchemy import create_engine, text
+
+
+# Function to parse emails from users.xlsx:
+def users_parser():
+    dict_users = dict()
+    wb = openpyxl.load_workbook('users_creator/users.xlsx')
+    sheet = wb.active
+    i = 1
+    for row in sheet.iter_rows(min_row=2, values_only=True):
+        first_name, last_name, email = row
+        dict_users[f'first_name_{i}'] = first_name
+        dict_users[f'last_name_{i}'] = last_name
+        dict_users[f'email_{i}'] = email
+        i += 1
+    return dict_users
 
 
 # Function to make browser types callable from terminal:
@@ -22,18 +42,28 @@ def pytest_addoption(parser):
 def browser(request):
     browser_name = request.config.getoption('--browser')
     if browser_name == 'chrome':
-        browser = webdriver.Chrome(ChromeDriverManager().install())
+        service = webdriver.chrome.service.Service(executable_path=ChromeDriverManager().install())
+        options = webdriver.ChromeOptions()
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--no-sandbox")
+        browser = webdriver.Chrome(service=service, options=options)
     elif browser_name == 'firefox':
-        browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+        service = webdriver.firefox.service.Service(executable_path=GeckoDriverManager().install())
+        options = webdriver.FirefoxOptions()
+        browser = webdriver.Firefox(service=service, options=options)
     elif browser_name == 'edge':
-        browser = webdriver.Edge(EdgeChromiumDriverManager().install())
+        service = webdriver.edge.service.Service(EdgeChromiumDriverManager().install())
+        options = webdriver.EdgeOptions()
+        options.use_chromium = True
+        browser = webdriver.Edge(service=service, options=options)
     else:
         raise pytest.UsageError('==browser_name should be chrome, firefox or edge')
+    browser.maximize_window()
     yield browser
     browser.quit()
 
 
-# Function to open Retro tab
+# Function to open Retro tab:
 @pytest.fixture(scope="function")
 def retro(browser):
     wait = WebDriverWait(browser, 20)
@@ -41,31 +71,27 @@ def retro(browser):
     # Open http://localhost:3000/:
     browser.get(link_home)
 
-    # Wait until "Email" input field is loaded on the page:
-    wait.until(EC.presence_of_element_located(
-        (By.XPATH, '//input[@placeholder="Enter Your Email"]'))
-    )
-
     # Fill in "Email" field:
-    browser.find_element(By.XPATH, '//input[@placeholder="Enter Your Email"]').send_keys('board_owner@mail.com')
+    wait.until(EC.presence_of_element_located(
+        (By.CSS_SELECTOR, 'input[placeholder="Enter Your Email"]'))
+    ).send_keys(users_parser()['email_1'])
+
 
     # Fill in "Password" field:
-    browser.find_element(By.XPATH, '//input[@placeholder="Enter Your Password"]').send_keys('Godfather1989!')
+    browser.find_element(By.CSS_SELECTOR, 'input[placeholder="Enter Your Password"]')\
+        .send_keys('Godfather1989!')
 
     # Click "Sign in" button:
     browser.find_element(By.CSS_SELECTOR, '.Button__button.Button__blue.styles__actionButton').click()
 
-    # Wait for "Not Found" x button to be displayed:
+    # Wait for "Not Found" button to be displayed:
     wait.until(EC.presence_of_element_located(
-        (By.XPATH, '//div[@role="alert"]')
-    ))
-    wait.until(EC.presence_of_element_located(
-        (By.XPATH, '//*[@id="root"]/div[2]/div/div/div/div/div/button')
+        (By.CSS_SELECTOR, 'div[role="alert"]')
     ))
 
     # Close "Not Found" alert:
-    if len(browser.find_elements(By.XPATH, '//div[@role="alert"]')) != 0:
-        alert = browser.find_element(By.XPATH, '//*[@id="root"]/div[2]/div/div/div/div/div/button')
+    if len(browser.find_elements(By.CSS_SELECTOR, 'div[role="alert"]')) != 0:
+        alert = browser.find_element(By.CSS_SELECTOR, 'div[role="alert"] > div > button')
         browser.execute_script("arguments[0].scrollIntoView();", alert)
         alert.click()
 
@@ -74,36 +100,87 @@ def retro(browser):
         browser.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/div/div[2]/div[3]/button').click()
 
     # Click "Retrospective" tab:
-    browser.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[1]/nav/a[3]/div').click()
+    browser.find_element(By.CSS_SELECTOR, 'a[href="/retro"]').click()
 
 
-# Function to open Feedback tab
+# Function to open Feedback tab:
 @pytest.fixture(scope="function")
 def feedback(browser, retro):
     wait = WebDriverWait(browser, 20)
 
     # Click "Feedback" button:
-    browser.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[1]/nav/div[3]/button[1]/div').click()
+    browser.find_element(By.CSS_SELECTOR, 'div[data-testid="header-menu-icon-wrapper"] >button:first-child').click()
 
-    # Check if opened dialog title is displayed:
-    wait.until(EC.presence_of_element_located(
-        (By.CSS_SELECTOR, '.ant-modal-header')
+# Function to click Scale button in Feedback tab:
+@pytest.fixture(scope="function")
+def feedback_input(browser, feedback):
+    wait = WebDriverWait(browser, 20)
+
+    # Click on 1 scale button:
+    wait.until(EC.element_to_be_clickable(
+        (By.CSS_SELECTOR, 'label[for="point1"] > div')
+    )).click()
+
+    # Check If "Send" button becomes available:
+    wait.until(EC.element_to_be_clickable(
+        (By.CSS_SELECTOR, '.ant-modal-footer .Button__button.Button__limeGreen')
     ))
+    assert browser.find_element(By.CSS_SELECTOR, '.ant-modal-footer .Button__button.Button__limeGreen').is_enabled(), \
+        'Send button is disabled'
 
 
+# Function to create board:
 @pytest.fixture(scope="function")
 def board_create(browser, retro):
     wait = WebDriverWait(browser, 20)
 
     # Click on "Create board" button:
     wait.until(EC.presence_of_element_located(
-        (By.XPATH, '//*[@id="root"]/div[1]/div[2]/div/div[1]/button')
-    ))
-    browser.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[2]/div/div[1]/button').click()
+        (By.CSS_SELECTOR, '.BoardManagement__boardManagementHeader > button')
+    )).click()
 
     # Fill in "Board name":
-    browser.find_element(By.XPATH, '//input[@placeholder="Board name"]')\
-        .send_keys('Board #1')
+    wait.until(EC.presence_of_element_located(
+        (By.CSS_SELECTOR, 'input[placeholder="Board name"]')
+    )).send_keys('Board #1')
 
     # Click "Create" button:
     browser.find_element(By.XPATH, '//button[text()="Create"]').click()
+
+    # Wait pop-up loading:
+    wait.until(EC.presence_of_element_located(
+        (By.XPATH, '//*[@id="root"]/div[2]/div/div/div/div/div/div/div')
+    ))
+
+
+@pytest.fixture(scope="function")
+def board_add_member(browser, board_create):
+    wait = WebDriverWait(browser, 20)
+
+    # Click "Manage members" button:
+    wait.until(EC.presence_of_element_located(
+        (By.CSS_SELECTOR, '.BoardPage__usersPanel > button')
+    )).click()
+
+    # Fill in member email:
+    wait.until(EC.presence_of_element_located(
+        (By.CSS_SELECTOR, '.TextInput__textInput.ManageUsersModal__emailInput')
+    )).send_keys(users_parser()['email_2'])
+
+    # Click "Add" button:
+    browser.find_element(By.CSS_SELECTOR, '.ManageUsersModal__addMember > button').click()
+
+    # Click "Save Changes" button:
+    browser.find_element(By.CSS_SELECTOR, '.ant-modal-footer > .Button__button.Button__limeGreen').click()
+
+
+# Function for database setup:
+@pytest.fixture(scope='function')
+def feedback_database_setup():
+    # DB connect:
+    engine = create_engine('postgresql://postgres:postgres@localhost:54324/agl_db')
+    connection = engine.connect()
+    yield connection
+    # DB close
+    connection.close()
+
--- users_creator/users_creator.py
+++ users_creator/users_creator.py
@@ -7,6 +7,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from testdata import *
 
 
+# Function to identify total users amount:
 def row_count():
     count = 0
     wb = openpyxl.load_workbook('users.xlsx')
@@ -18,6 +19,7 @@ def row_count():
     return count
 
 
+# Function to create dictionary with all the users in .xlsx:
 def users_parser():
     dict_users = dict()
     wb = openpyxl.load_workbook('users.xlsx')
@@ -31,6 +33,7 @@ def users_parser():
         i += 1
     return dict_users
 
+# Function to create all users from .xlsx in AgileS app:
 def user_creator():
     for i in range(1, row_count()):
         name = users_parser()[f'first_name_{i}']
@@ -40,20 +43,18 @@ def user_creator():
         wait = WebDriverWait(browser, 20)
         browser.get(link_home)
         w_agiles = browser.current_window_handle
-        wait.until(EC.presence_of_element_located((By.LINK_TEXT, "Sign Up")))
-        browser.find_element(By.LINK_TEXT, "Sign Up").click()
-        browser.find_element(By.XPATH, "//input[@placeholder='Enter First Name']").send_keys(name)
-        browser.find_element(By.XPATH, "//input[@placeholder='Enter Last Name (Optional)']").send_keys(surname)
-        browser.find_element(By.XPATH, "//input[@placeholder='Enter Your Email']").send_keys(email)
-        browser.find_element(By.XPATH, "//input[@placeholder='Enter Your Password']").send_keys('G')
-        browser.find_element(By.XPATH, "//input[@placeholder='Enter Your Password']").send_keys('odfather1')
-        browser.find_element(By.XPATH, "//input[@placeholder='Enter Your Password']").send_keys('989!')
-        browser.find_element(By.XPATH, "//input[@placeholder='Confirm Password']").send_keys('Godfather1989!')
+        wait.until(EC.presence_of_element_located((By.LINK_TEXT, "Sign Up"))).click()
+        browser.find_element(By.CSS_SELECTOR, "input[placeholder='Enter First Name']").send_keys(name)
+        browser.find_element(By.CSS_SELECTOR, "input[placeholder='Enter Last Name (Optional)']").send_keys(surname)
+        browser.find_element(By.CSS_SELECTOR, "input[placeholder='Enter Your Email']").send_keys(email)
+        browser.find_element(By.CSS_SELECTOR, "input[placeholder='Enter Your Password']").send_keys('G')
+        browser.find_element(By.CSS_SELECTOR, "input[placeholder='Enter Your Password']").send_keys('odfather1')
+        browser.find_element(By.CSS_SELECTOR, "input[placeholder='Enter Your Password']").send_keys('989!')
+        browser.find_element(By.CSS_SELECTOR, "input[placeholder='Confirm Password']").send_keys('Godfather1989!')
         browser.find_element(By.CSS_SELECTOR, ".Button__button.Button__blue.styles__actionButton").click()
         browser.switch_to.new_window('tab')
         browser.get(link_admin)
-        wait.until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div/div[2]/div[2]/div[1]')))
-        browser.find_element(By.XPATH, '/html/body/div/div/div[2]/div[2]/div[1]').click()
+        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.msglist-message:first-child'))).click()
         auth_link = browser.find_element(By.PARTIAL_LINK_TEXT, 'http://localhost:8086')
         access_link = 'http://127.0.0.1:3000/' + auth_link.text[22:]
         browser.switch_to.window(w_agiles)
@@ -63,4 +64,5 @@ def user_creator():
         print(f'User {name} {surname}, email: {email} was created')
     print(f'{row_count() - 1} Users were created successfully')
 
+# launch User creator:
 user_creator()
